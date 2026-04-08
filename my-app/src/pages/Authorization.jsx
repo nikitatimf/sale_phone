d7c9bf3fdd87c9d61aadbc5829be3e46c9812b12
Nikita <dorgunnikita899@gmail.com>
2026-04-08 23:30:13 +0300

    added registration

--- my-app/src/pages/Authorization.jsx
+++ my-app/src/pages/Authorization.jsx
@@ -1,6 +1,36 @@
 import Registration from './registration'
 import { Router, Route, Link, Routes } from 'react-router-dom'
 
+const click = async () => {
+    try {
+        const emailValue = document.querySelector('#email').value;
+        const passwordValue = document.querySelector('#password').value;
+        const response = await fetch('http://localhost:5000/api/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: emailValue,
+                password: passwordValue
+            })
+        });
+
+        const data = await response.json();
+        
+        if (response.ok) {
+            console.log('Успех:', data);
+            // Сохраняем токен
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('user', JSON.stringify(data.user));
+        } else {
+            console.error('Ошибка:', data.message);
+        }
+    } catch (error) {
+        console.error('Ошибка запроса:', error);
+  }
+}
+
 const Authorization = () => {
     return (
         <div className='app'>
@@ -24,7 +54,7 @@ const Authorization = () => {
                         className='authorization_input'
                         placeholder='Password'
                     />
-                    <button className='authorization_button'>Log in</button>
+                    <button className='authorization_button' onClick={click}>Log in</button>
                     <Link to="/registration" className='link_authorization authorization_button authorization_button-registration'>
                         Registration
                     </Link>
--- my-app/src/pages/registration.jsx
+++ my-app/src/pages/registration.jsx
@@ -1,30 +1,110 @@
 
 import { Router, Route, Link, Routes } from 'react-router-dom'
 
+
+const validLight = () => {
+    const form = document.querySelector('form');
+    const requir = Array.from(form.querySelectorAll('[required]'));
+    let allValid = true;
+    requir.forEach(input => {
+        const isEmpty = input.value.trim() === '';
+        if (isEmpty) {
+            allValid = false;
+            input.classList.add('input_error');
+        }
+        else {
+            input.classList.remove('input_error');
+        }
+    })
+    return allValid;
+}
+
+const click = async (e) => {
+    try {
+        const isValid = validLight();
+        if (!isValid) {
+            e.preventDefault();
+            return;
+        }
+
+        const nameValue = document.querySelector('#name').value;
+        const emailValue = document.querySelector('#email').value;
+        const passwordValue = document.querySelector('#password').value;
+        const response = await fetch('http://localhost:5000/api/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: nameValue,
+                email: emailValue,
+                password: passwordValue
+            })
+        })
+
+        const data = await response.json();
+        console.log(data.message)
+    } catch {
+        console.log('error')
+    }
+}
+
 const Registration = () => {
     return (
         <div className='app'>
             <div className="container container-authorization">
                 Phone shop
-                <img className='app_authorization_photo' src="src/img/authorization.png"/>
+                <img className='app_authorization_photo' src="my-app/src/img/authorization.png"/>
                 <div className='authorization_area'>
-                    Log in
-                    <label htmlFor="email" className='authorization_area_label'></label>
-                    <input 
-                        id="email" 
-                        type="email" 
-                        className='authorization_input' 
-                        placeholder='Email'
-                    />
+                    Registration
+                    <form 
+                        action="register"
+                        className='form_auth'
+                    >
+                        <label 
+                            htmlFor="name" 
+                            className='authorization_area_label' 
+                        />
+                        <input 
+                            id="name" 
+                            type="text" 
+                            className='authorization_input' 
+                            placeholder='Name'
+                            required
+                        />
 
-                    <label htmlFor="password" className='authorization_area_label'></label>
-                    <input 
-                        id="password" 
-                        type="password" 
-                        className='authorization_input'
-                        placeholder='Password'
-                    />
-                    <button className='authorization_button'>Log in</button>
+                        <label 
+                            htmlFor="email" 
+                            className='authorization_area_label'
+                        />
+                        <input 
+                            id="email" 
+                            type="email" 
+                            className='authorization_input' 
+                            placeholder='Email'
+                            required
+                        />
+
+                        <label 
+                            htmlFor="password" 
+                            className='authorization_area_label' 
+                        />
+                        <input 
+                            id="password" 
+                            type="password" 
+                            className='authorization_input'
+                            placeholder='Password'
+                            required
+                        />
+
+                        <button 
+                            className='authorization_button'
+                            onClick={click}
+                            type='button'
+                        >
+                            Registration
+                        </button>
+                    </form>
                     {/* <Link to="/registration" className='link_authorization authorization_button authorization_button-registration'>
                         Registration
                     </Link> */}
